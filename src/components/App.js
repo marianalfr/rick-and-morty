5ee2208f33e36bc3f7796ad75a6e2ca7d7cd1b91
marianalfr <marianalfr@gmail.com>
2020-01-13 15:22:58 +0100

    Initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import '../stylesheets/App.css';
 import { fetchCharacters } from '../services/Api'
-import CharacterList from './CharacterList';
-import Search from './Search';
-import { Route, Switch } from 'react-router-dom';
-import CharacterDetails from './CharacterDetails';
 // import Loader from './Loader';
 
 class App extends React.Component {
