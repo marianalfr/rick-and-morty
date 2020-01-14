53119913c132d0551fa41f2418a3470fb6b140ec
marianalfr <marianalfr@gmail.com>
2020-01-14 10:47:56 +0100

    Back to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/App.scss';
 import App from './components/App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter basename="/modulo-3-evaluacion-final-marianalfr">
       <App />
-    </HashRouter>,
+    </BrowserRouter>,
     document.getElementById('root')
   );
